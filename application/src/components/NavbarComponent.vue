439db416c7a7a7416b6f12cc155cdde145cf2355
Filip <filip.stojsavljevic@gmail.com>
2022-06-01 23:08:55 +0200

    Rad na pocetnoj stranici

---
+++ application/src/components/NavbarComponent.vue
@@ -0,0 +1,55 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+
+    <img src="/images/logo.png" id="logoImg" alt="">
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="navbar-nav">
+          <router-link class="nav-item" to="/">Početna stranica</router-link>&nbsp;&nbsp;&nbsp;
+          <router-link class="nav-item" to="/zivotinje">Zivotinje</router-link>&nbsp;&nbsp;&nbsp;
+          <router-link class="nav-item" to="/izgubljeni">Izgubljeni ljubimci</router-link>&nbsp;&nbsp;&nbsp;
+          <router-link class="nav-item" to="/dodaj">Dodaj oglas</router-link>&nbsp;&nbsp;&nbsp;
+          <router-link class="nav-item" to="/nalog">Moj nalog</router-link>&nbsp;&nbsp;&nbsp;
+          <router-link class="nav-item" to="/o_nama">O nama</router-link>&nbsp;&nbsp;&nbsp;
+
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<style lang="less">
+
+#logoImg {
+  width: 100px;
+}
+
+nav {
+  padding: 30px;
+  background-color: #83C5BE;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #83C5BE;
+    }
+  }
+}
+
+.nav-item {
+  padding: 20px;
+}
+</style>
+
+<script>
+export default {
+    name: 'NavbarComponent'
+}
+</script>
--- application/src/views/HomeView.vue
+++ application/src/views/HomeView.vue
@@ -1,8 +1,46 @@
 <template>
-  <div class="home">
-      <h1>Pocetna stranica</h1>
+  <div class="home row">
+      <h3>Učini pametnu stvar i udomi neku kucu, macu ili ptičicu!</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam hendrerit molestie tortor, a volutpat nibh consequat eu. Nullam gravida, velit id fringilla malesuada, ipsum odio sollicitudin metus, in finibus nibh turpis ac nibh. Donec congue sagittis justo, eget sodales sapien euismod et. Proin leo massa, iaculis id ultricies non, vehicula elementum dui. 
+      </p>
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/images/dog.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="/images/cat.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="/images/bird.png" class="d-block w-100" alt="...">
+          </div>
+        </div>
+      <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </button>
+      </div>
   </div>
 </template>
+
+<style scoped>
+  .home {
+    padding: 30px;
+    margin: 30px;
+    background-color: #FFDDD2;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+
+  .home p {
+    text-align: left;
+  }
+</style>
 
 <script>
 // @ is an alias to /src
